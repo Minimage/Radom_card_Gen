e6d20b04291a17fdb6362abc4fd985ea7d02a596
Minimage <seancampbell914@gmail.com>
2022-04-13 02:47:57 +0000

    Structure done, Spacing, positioning, and color needs to be worked on

--- src/app.js
+++ src/app.js
@@ -7,5 +7,17 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let randomSuite = Math.floor(Math.random() * 3);
+
+  //Will need to turn 1:Ace and 11-13 into Jack, Queen and King
+  let randomNumber = Math.floor(Math.random() * 9) + 1;
+  let num = document.getElementById("number");
+  num.innerHTML = randomNumber;
+
+  const classes = ["♦", "♥", "♠", "♣"];
+  let suite = classes[randomSuite];
+  let topSuite = document.getElementById("top");
+  let botSuite = document.getElementById("bot");
+  topSuite.innerHTML = suite;
+  botSuite.innerHTML = suite;
 };
